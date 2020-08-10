065e8dc1b68e3502a772ab59d6ebe3b2e32ca48d
tfoster-ut <68560386+tfoster-ut@users.noreply.github.com>
2020-08-10 16:32:47 -0500

    added file and tested ifs

---
+++ Python_ifElse_Test.py
@@ -0,0 +1,7 @@
+
+temperature = int(input("What is the temperature outside?"))
+
+if temperature > 80:
+    print("Turn on the AC")
+else:
+    print("Open the windows")
--- Python_practice.py
+++ Python_practice.py
@@ -1,16 +1,5 @@
 
-voting_data = []
+counties = ["Arapahoe", "Denver", "Jefferson"]
 
-voting_data.append({"county": "Arapahoe", "registered_voters": 422829})
-voting_data.append({"county": "Denver", "registered_voters": 463353})
-voting_data.append({"county": "El Paso", "registered_voters": 461149})
-voting_data.append({"county": "Jefferson", "registered_voters": 432438})
-
-voting_data.pop(0)
-
-voting_data.insert(2,voting_data.pop(0))
-voting_data.insert(1, voting_data.pop(1))
-
-voting_data.append({"county": "Arapahoe", "registered_voters": 422829})
-
-print(voting_data)
+if counties[1] == 'Denver':
+    print(counties[1])
