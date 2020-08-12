9625facf3575025619b9374ee93556088b285dd6
Teale Foster <LivingLegend@Teales-MBP.lan1>
2020-08-12 10:27:34 -0500

    removed old files

--- Python_loops.py
+++
@@ -1,5 +0,0 @@
-
-
-counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
-for county, voters in counties_dict.items():
-    print(f"{county} county has {voters} registered voters.")
