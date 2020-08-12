e074089912ce7881f713d6805a37b01d8b855f1d
Teale Foster <LivingLegend@Teales-MBP.lan1>
2020-08-12 14:46:52 -0500

    read csv and identify headers

--- PyPoll.py
+++ PyPoll.py
@@ -26,7 +26,10 @@ file_to_save = os.path.join('analysis', 'election_analysis.txt')
 with open(file_to_load) as election_data:
 
 #To Do: performa analysis
-with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+    headers = next(file_reader)
+    print(headers)
+
 
 
 
