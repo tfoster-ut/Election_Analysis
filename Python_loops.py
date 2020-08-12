8b32aedd4308896b90c2f4ff2438f509373305d8
Teale Foster <LivingLegend@Teales-MBP.lan1>
2020-08-12 10:05:02 -0500

    completed 2 skill drills

--- Python_loops.py
+++ Python_loops.py
@@ -1,15 +1,5 @@
 
 
-counties = ["Arapahoe","Denver","Jefferson"]
-
-voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
-                {"county":"Denver", "registered_voters": 463353},
-                {"county":"Jefferson", "registered_voters": 432438}]
-
-counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
-
-#Using .items in a loop
-
-#for county, voters in counties_dict.items():
-#    print(county, "county has", voters, "registered voters")
-
+counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters} registered voters.")
