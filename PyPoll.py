0769e3b442966d4fece40a0c459f7f58cb7bf22e
Teale Foster <LivingLegend@Teales-MBP.lan1>
2020-08-12 12:53:32 -0500

    added pseudocode and trial line

---
+++ PyPoll.py
@@ -0,0 +1,21 @@
+
+# The data that needs to be collected
+
+# 1) Total votes cast
+
+# 2) Comprehensive list of candidates that received votes
+
+# 3) Percantage of votes that each candidate won
+
+# 4) Total number of votes each candidate won
+
+# 5) The winning candidate by total popular vote
+
+
+import datetime
+
+now = datetime.datetime.now()
+
+print(f'The current date and time is {now:}')
+
+
